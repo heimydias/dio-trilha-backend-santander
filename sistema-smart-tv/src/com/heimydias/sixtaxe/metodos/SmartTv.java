31b26638685cf3f15b8ac6d36305efb48a2a197c
heimydias <heimysantana@hotmail.com>
2023-09-06 14:26:32 -0300

    exercicio sobre metodos

--- sistema-smart-tv/src/com/heimydias/sixtaxe/metodos/SmartTv.java
+++ sistema-smart-tv/src/com/heimydias/sixtaxe/metodos/SmartTv.java
@@ -1,2 +1,38 @@
-package com.heimydias.sixtaxe.metodos;public class SmartTv {
+package com.heimydias.sixtaxe.metodos;
+
+public class SmartTv {
+    boolean ligada = false;
+    int canal = 1;
+    int volume = 25;
+
+    public void ligar(){
+        ligada = true;
+    }
+    public void desligada(){
+        ligada = false;
+    }
+
+    public void aumentarVolume(){
+        volume++;
+        System.out.println("Aumentando volume: " + volume);
+    }
+
+    public void diminuirVolume(){
+        volume--;
+        System.out.println("Diminuindo volume: " + volume);
+    }
+
+    public void aumentarCanal(){
+        canal++;
+        System.out.println("Canal: " + canal);
+    }
+
+    public void diminuirCanal() {
+        canal--;
+        System.out.println("Canal: " + canal);
+    }
+
+    public void mudarCanal(int novoCanal){
+        canal = novoCanal;
+    }
 }
--- sistema-smart-tv/src/com/heimydias/sixtaxe/metodos/Usuario.java
+++ sistema-smart-tv/src/com/heimydias/sixtaxe/metodos/Usuario.java
@@ -1,2 +1,29 @@
-package com.heimydias.sixtaxe.metodos;public class Usuario {
+package com.heimydias.sixtaxe.metodos;
+
+public class Usuario {
+    public static void main(String[] args) {
+
+        SmartTv smartTv = new SmartTv();
+        System.out.println("Tv ligada? " + smartTv.ligada);
+        System.out.println("Canal atual: " + smartTv.canal);
+        System.out.println("Volume atual: " + smartTv.volume);
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+
+        smartTv.mudarCanal(13);
+
+        smartTv.ligar();
+        System.out.println("Novo status -> Tv ligada? " + smartTv.ligada);
+
+        smartTv.desligada();
+        System.out.println("Novo status -> Tv ligada? " + smartTv.ligada);
+
+        System.out.println("volume atual: " + smartTv.volume);
+
+        System.out.println("Canal atual: " + smartTv.canal);
+
+    }
 }
