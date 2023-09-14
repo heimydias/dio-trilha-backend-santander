f66dcaad40b6f24d6fae935dcf6ba339c001a65d
heimydias <heimysantana@hotmail.com>
2023-09-14 19:00:48 -0300

    aula sobre nomenclatura pacotes

---
+++ exemplo-pacotes/src/com/mastersoft/clinical/service/ExameService.java
@@ -0,0 +1,12 @@
+package com.mastersoft.clinical.service;
+
+import com.mastersoft.clinical.model.Exame;
+
+public class ExameService {
+    //podem conter métodos que realizam operações de negócios específicas,
+    // como validações, cálculos complexos e coordenação de chamadas ao banco
+    // de dados (usando classes de repositório) para recuperar ou persistir dados.
+    void salvarExame(Exame exame){
+
+    }
+}
