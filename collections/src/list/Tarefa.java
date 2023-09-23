06d8810f794d5dd513b847a8204a648ba622be74
heimydias <heimysantana@hotmail.com>
2023-09-23 00:22:33 -0300

    praticando list

--- collections/src/list/Tarefa.java
+++ collections/src/list/Tarefa.java
@@ -1,2 +1,13 @@
-package list;public class Tarefa {
+package list;
+
+public class Tarefa {
+    private String descricao;
+
+    public Tarefa(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
 }
