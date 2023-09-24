d24f8c05db61061436d58b69e09ede61ba2f5a96
heimydias <heimysantana@hotmail.com>
2023-09-23 22:38:07 -0300

    praticando list

---
+++ collections/src/list/CatalagoLivros.java
@@ -0,0 +1,68 @@
+package list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CatalagoLivros {
+
+    private List<Livro> livroList;
+
+    public CatalagoLivros() {
+        this.livroList = new ArrayList<>();
+    }
+
+    public void adicionarLivro(String titulo, String autor, int anoPublicacao) {
+        livroList.add(new Livro(titulo, autor, anoPublicacao));
+    }
+
+    public List<Livro> pesquisarAutor(String autor) {
+        List<Livro> livrosAutor = new ArrayList<>();
+        if (!livroList.isEmpty()) {
+            for (Livro l : livroList) {
+                if (l.getAutor().equalsIgnoreCase(autor)) {
+                    livrosAutor.add(l);
+                }
+            }
+        }
+        return livrosAutor;
+    }
+
+    public List<Livro> pesquisarIntervaloAnos(int anoInicial, int anoFinal) {
+        List<Livro> livrosIntevaloAnos = new ArrayList<>();
+        if (!livroList.isEmpty()) {
+            for (Livro l : livroList) {
+                if (l.getAnoPublicacao() >= anoInicial && l.getAnoPublicacao() <= anoFinal) {
+                    livrosIntevaloAnos.add(l);
+                }
+            }
+        }
+        return livrosIntevaloAnos;
+    }
+
+    public Livro pesquisarTitulo(String titulo) {
+        Livro livroTitulo = null;
+        if (!livroList.isEmpty()){
+            for (Livro l : livroList){
+                if (l.getTitulo().equalsIgnoreCase(titulo)){
+                    livroTitulo = l;
+                    break;
+                }
+            }
+        }
+        return livroTitulo;
+    }
+
+    public static void main(String[] args) {
+        CatalagoLivros catalagoLivros = new CatalagoLivros();
+
+        catalagoLivros.adicionarLivro("Livro 1", "Autor 1", 2020);
+        catalagoLivros.adicionarLivro("Livro 1", "Autor 2", 2021);
+        catalagoLivros.adicionarLivro("Livro 2", "Autor 2", 2022);
+        catalagoLivros.adicionarLivro("Livro 3", "Autor 3", 2023);
+        catalagoLivros.adicionarLivro("Livro 4", "Autor 4", 1994);
+
+        System.out.println(catalagoLivros.pesquisarAutor("Autor 2"));
+        System.out.println(catalagoLivros.pesquisarIntervaloAnos(2020,2022));
+        System.out.println(catalagoLivros.pesquisarTitulo("Livro 1"));
+    }
+}
